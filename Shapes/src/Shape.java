639564d41b7cd1b55a271cc2ca8083b274f54f35
Aidaiamanbekova <145114212+Aidaiamanbekova@users.noreply.github.com>
2023-10-04 11:28:41 +0600

    Add files via upload

---
+++ Shapes/src/Shape.java
@@ -0,0 +1,14 @@
+abstract class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public String getName() {
+        return name;
+    }
+}
